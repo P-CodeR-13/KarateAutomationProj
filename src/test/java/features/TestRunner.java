cdb063c87498b8cdb23cf6d98360026e0f6dff47
P-CodeR-13 <paolo.retoran13@gmail.com>
2024-10-25 16:57:57 +0800

    Initial commit

---
+++ src/test/java/features/TestRunner.java
@@ -0,0 +1,12 @@
+package features.users;
+
+import com.intuit.karate.junit5.Karate;
+
+class TestRunner {
+    
+    @Karate.Test
+    Karate testUsers() {
+        return Karate.run("features").relativeTo(getClass());
+    }    
+
+}
---
+++ src/test/java/features/testDemoQA.feature
@@ -0,0 +1,8 @@
+
+Feature: Test Demo QA
+
+  Scenario: Go to TestDemoQA
+
+    Given driver url
+    * driver.maximize()
+
